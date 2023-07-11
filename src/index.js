3cb6f7ac0d957703ca0f0bb306180dc53a51b003
Om Kale <omkale23@gmail.com>
2023-07-11 22:13:06 +0530

    add apis for crud

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { PrismaClient } from '@prisma/client'
-import { hash, verify } from "argon2";
+import { hash, verify as argonVerify } from "argon2";
+import cors from "cors";
 import { validation } from "./middlewares.mjs";
 import sign from "jsonwebtoken/sign.js";
+import verify from "jsonwebtoken/verify.js";
 
 
 const app = express();
 app.use(express.json())
+app.use(cors())
 
 const prisma = new PrismaClient()
 
@@ -52,7 +55,7 @@ app.post("/login", async (req, res) => {
       return res.status(404).json({error: "User does not exist"})
     }
 
-    const isCorrectPassword = verify(user.password, password)
+    const isCorrectPassword = await argonVerify(user.password, password)
 
     if(!isCorrectPassword) {
       return res.status(401).json({error: "Incorrect password"})
@@ -60,6 +63,80 @@ app.post("/login", async (req, res) => {
     const token = sign({mobile}, process.env.SECRET, { expiresIn: '1800s' })
     return res.json({token})
   } catch (error) {
+    return res.status(500).json({response: "Something went wrong"})
+  }
+})
+
+app.get("/my-profile", async (req, res)=> {
+  try {
+    const token = req.headers["authorization"].split(" ")[1]
+    if(!token) {
+      return res.status(401).json({msg: "Token not found"})
+    }
+    const data = verify(token, process.env.SECRET)
+    const mobile = data.mobile
+
+    const user = await prisma.user.findFirst({
+      where: {
+        mobile
+      }
+    })
+
+    return res.json({user})
+
+  } catch (error) {
+    return res.status(500).json({response: "Something went wrong"})
+  }
+})
+
+app.put("/edit-profile", async (req, res) => {
+  try {
+    const {firstName, lastName} = req.body;
+    const token = req.headers["authorization"].split(" ")[1]
+    if(!token) {
+      return res.status(401).json({msg: "Token not found"})
+    }
+    const data = verify(token, process.env.SECRET)
+    const mobile = data.mobile
+
+    const user = await prisma.user.update({
+      where: {
+        mobile
+      },
+      data: {
+        firstName,
+        lastName
+      }
+    })
+
+    return res.json({user, msg: "user updated"})
+
+  } catch (error) {
+    console.error({error})
+    return res.status(500).json({response: "Something went wrong"})
+  }
+})
+
+app.delete("/delete-profile", async (req, res)=>{
+  try {
+    const {firstName, lastName} = req.body;
+    const token = req.headers["authorization"].split(" ")[1]
+    if(!token) {
+      return res.status(401).json({msg: "Token not found"})
+    }
+    const data = verify(token, process.env.SECRET)
+    const mobile = data.mobile
+
+    const user = await prisma.user.delete({
+      where: {
+        mobile
+      }
+    })
+
+    return res.json({msg: "user deleted"})
+
+  } catch (error) {
+    console.error({error})
     return res.status(500).json({response: "Something went wrong"})
   }
 })
